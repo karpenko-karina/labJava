a1988e1f43bb7af048ff3507a52db0eda774b952
karpenko-karina <karpenkokarina.k@gmail.com>
2022-10-13 02:25:33 +0300

    task three done

---
+++ lab1/t3/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("fibNum(92) = " + fibNum.fibNum(92) + "\n");
+        System.out.print("fibNum(50) = " + fibNum.fibNum(50) + "\n");
+        System.out.print("fibNum(13) = " + fibNum.fibNum(13) + "\n");
+    }
+}
---
+++ lab1/t3/src/fibNum.java
@@ -0,0 +1,14 @@
+public class fibNum {
+    private static long[] arr = new long[92];
+        private static int last = 2;
+    static {
+        arr[0] = 1;
+        arr[1] = 1;
+    }
+    public static long fibNum(int n) {
+        for (int i = last; i < n; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+        return arr[n - 1];
+    }
+}
